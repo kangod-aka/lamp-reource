0aee3f1749f194fb3956371a2cd220b1199f2817
KangShen <yk625x@163.com>
2023-06-18 09:52:38 +0800

    3、初始导入

--- lamp-web-plus/src/views/lamp/more/multiMenu/menu1-1/menu1-1-1/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding: 30px;"> 我是菜单1-1-1 </div>
-</template>
-<script lang="ts">
-  import { defineComponent } from 'vue';
-  export default defineComponent({ name: '菜单1-1-1' });
-</script>
--- lamp-web-plus/src/views/lamp/workbench/started/index.vue
+++
@@ -1,3 +0,0 @@
-<template functional>
-  <div style="padding: 30px"> 联系作者开通企业版后，获取此项目源码 </div>
-</template>
